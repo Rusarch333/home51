a7fd1b5f38df8b0199f32a1c90a4d34f1c6b5bbd
Lite <rusarch333@gmail.com>
2024-05-24 01:04:09 +0300

    Remove targetButton innerText toLowerCase

--- index.js
+++ index.js
@@ -17,14 +17,14 @@ for (const flexDirectionButton of flexDirectionButtons) {
     for (const flexDirectionButton of flexDirectionButtons) {
       flexDirectionButton.classList.remove(FLEX_DIR_BTN_ACTIVE_CL_NAME);
     }
-    
+
     // Додаємо клас активної кнопки до кнопки, по якій клікнули
     targetButton.classList.add(FLEX_DIR_BTN_ACTIVE_CL_NAME);
 
     // Змінюємо флекс-направлення флекс-контейнера, беручи нове значення з тексту кнопки, по якій клікнули
     flexDirectionArea.style.setProperty(
       'flex-direction',
-      targetButton.innerText.toLowerCase()
+      targetButton.innerText
     );
   });
 }
