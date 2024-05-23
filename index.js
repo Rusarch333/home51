cc55acae3b3a6b98995150ae7ffdeee56b376c44
Lite <rusarch333@gmail.com>
2024-05-24 00:53:37 +0300

    Improve visual looking of code

--- index.js
+++ index.js
@@ -12,13 +12,15 @@ const [flexDirectionArea] = document.getElementsByClassName(
 );
 
 for (const flexDirectionButton of flexDirectionButtons) {
-  flexDirectionButton.addEventListener('click', ({target: targetButton}) => {
+  flexDirectionButton.addEventListener('click', ({ target: targetButton }) => {
     // При кліку на кнопку видаляємо у всіх кнопок клас активної кнопки
     for (const flexDirectionButton of flexDirectionButtons) {
       flexDirectionButton.classList.remove(FLEX_DIR_BTN_ACTIVE_CL_NAME);
     }
+    
     // Додаємо клас активної кнопки до кнопки, по якій клікнули
     targetButton.classList.add(FLEX_DIR_BTN_ACTIVE_CL_NAME);
+
     // Змінюємо флекс-направлення флекс-контейнера, беручи нове значення з тексту кнопки, по якій клікнули
     flexDirectionArea.style.setProperty(
       'flex-direction',
